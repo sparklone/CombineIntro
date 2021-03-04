44bf7b4dbcf527ec8e04f64b0184309df5c27038
Aleksandr Glushchenko <sparklone@gmail.com>
2021-03-04 23:26:47 +0300

    Initial Commit

---
+++ CombineIntro/CombineIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineIntroApp.swift
+//  CombineIntro
+//
+//  Created by Alexandr Glushchenko on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CombineIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineIntro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineIntro
+//
+//  Created by Alexandr Glushchenko on 04.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
